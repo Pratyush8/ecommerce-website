202674489ea2676d7caea90c9b22eaf7c99d147a
Pratyush saxena <54858004+Pratyush8@users.noreply.github.com>
2021-11-26 22:27:46 +0530

    Add files via upload

---
+++ src/component/footer/Footer.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer jusify-content-center align-items-center ">
+      <ul>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#">Shop</a>
+        </li>
+        <li>
+          <a href="#">Help</a>
+        </li>
+      </ul>
+      <h5>&copy; E-MART </h5>
+    </div>
+  );
+};
+
+export default Footer;
